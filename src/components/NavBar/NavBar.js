1b2f3b3376294a1239e51a0e0469a218f6c7d0d8
Lucas Chaves <lunechaves@gmail.com>
2023-05-30 16:24:35 -0300

    Entrega1

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,27 +1,33 @@
+import "./NavBar.css"
 import CartWidget from '../CartWidget/CartWidget';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const NavBar = () => {
     return (
         <div className="container">
-            <nav className="nav">
-                <div className="nav_brand">
-                    <a className="nav_link" href="#">Ecommerce</a>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">E-commerce</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" aria-current="page" href="#">Inicio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Productos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contacto</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">0 <CartWidget /></a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <ul className="nav_list">
-                    <li>
-                        <a className="nav_link" href="#">Inicio</a>
-                    </li>
-                    <li>
-                        <a className="nav_link" href="#">Productos</a>
-                    </li>
-                    <li>
-                        <a className="nav_link" href="#">Contacto</a>
-                    </li>
-                    <li>
-                        <a className="nav_link" href="#"><CartWidget /></a>
-                    </li>
-                </ul>
             </nav>
         </div>
     )
